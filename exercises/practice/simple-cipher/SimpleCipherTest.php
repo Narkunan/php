a7bf99c6a2819b16fdfa1c41defd371029bb82b5
narkunan <narkunan9585@gmail.com>
2026-07-18 20:30:16 +0530

    Removed dot from uuid and updated method names to match doc

--- exercises/practice/simple-cipher/SimpleCipherTest.php
+++ exercises/practice/simple-cipher/SimpleCipherTest.php
@@ -17,10 +17,10 @@ class SimpleCipherTest extends TestCase
      * output the key. This is a critical problem with shift ciphers, some
      * characters will always output the key verbatim.
      *
-     * Uuid: b8bdfbe1-bea3-41bb-a999-b41403f2b15d.
+     * Uuid: b8bdfbe1-bea3-41bb-a999-b41403f2b15d
      */
     #[TestDox('Random key cipher -> Can encode')]
-    public function testRandomKeyCipherEncode(): void
+    public function testRandomKeyCipherCanEncode(): void
     {
         $cipher = new SimpleCipher();
         $plaintext = 'aaaaaaaaaa';
@@ -28,42 +28,42 @@ class SimpleCipherTest extends TestCase
     }
 
     /**
-   * Uuid: 3dff7f36-75db-46b4-ab70-644b3f38b81c.
-   */
+     * Uuid: 3dff7f36-75db-46b4-ab70-644b3f38b81c
+     */
     #[TestDox('Random key cipher -> Can decode')]
-    public function testRandomKeyCipherDecode(): void
+    public function testRandomKeyCipherCanDecode(): void
     {
         $cipher = new SimpleCipher();
         $plaintext = 'aaaaaaaaaa';
         $this->assertEquals($plaintext, $cipher->decode(substr($cipher->key, 0, 10)));
     }
 
-  /**
-   * Uuid: 8143c684-6df6-46ba-bd1f-dea8fcb5d265.
-   */
-    #[TestDox('Random key cipher -> Is reversible. I.e., if you apply decode in a encoded result, you must see the same plaintext encode parameter as a result of the decode method')]
-    public function testRandomKeyCipherReversible(): void
+    /**
+     * Uuid: 8143c684-6df6-46ba-bd1f-dea8fcb5d265
+     */
+    #[TestDox('Random key cipher -> Is reversible')]
+    public function testRandomKeyCipherIsReversible(): void
     {
         $cipher = new SimpleCipher();
         $plaintext = 'abcdefghij';
         $this->assertEquals($plaintext, $cipher->decode($cipher->encode($plaintext)));
     }
 
-     /**
-   * Uuid: defc0050-e87d-4840-85e4-51a1ab9dd6aa.
-   */
+    /**
+     * Uuid: defc0050-e87d-4840-85e4-51a1ab9dd6aa
+     */
     #[TestDox('Random key cipher -> Key is made only of lowercase letters')]
-    public function testRandomCipherKeyIsLetters(): void
+    public function testRandomCipherKeyIsLowercaseLetters(): void
     {
         $cipher = new SimpleCipher();
         $this->assertMatchesRegularExpression('/\A[a-z]+\z/', $cipher->key);
     }
 
     /**
-   * Uuid: 565e5158-5b3b-41dd-b99d-33b9f413c39f.
-   */
+     * Uuid: 565e5158-5b3b-41dd-b99d-33b9f413c39f
+     */
     #[TestDox('Substitution cipher -> Can encode')]
-    public function testCipherEncode(): void
+    public function testSubstitutionCipherCanEncode(): void
     {
         $cipher = new SimpleCipher('abcdefghij');
         $plaintext = 'aaaaaaaaaa';
@@ -72,10 +72,10 @@ class SimpleCipherTest extends TestCase
     }
 
     /**
-   * Uuid: d44e4f6a-b8af-4e90-9d08-fd407e31e67b.
-   */
+     * Uuid: d44e4f6a-b8af-4e90-9d08-fd407e31e67b
+     */
     #[TestDox('Substitution cipher -> Can decode')]
-    public function testCipherDecode(): void
+    public function testSubstitutionCipherCanDecode(): void
     {
         $cipher = new SimpleCipher('abcdefghij');
         $plaintext = 'aaaaaaaaaa';
@@ -84,10 +84,10 @@ class SimpleCipherTest extends TestCase
     }
 
     /**
-   * Uuid: 70a16473-7339-43df-902d-93408c69e9d1.
-   */
-    #[TestDox('Substitution cipher -> Is reversible. I.e., if you apply decode in a encoded result, you must see the same plaintext encode parameter as a result of the decode method')]
-    public function testCipherReversible(): void
+     * Uuid: 70a16473-7339-43df-902d-93408c69e9d1
+     */
+    #[TestDox('Substitution cipher -> Is reversible')]
+    public function testSubstitutionCipherIsReversible(): void
     {
         $cipher = new SimpleCipher('abcdefghij');
         $plaintext = 'abcdefghij';
@@ -95,10 +95,10 @@ class SimpleCipherTest extends TestCase
     }
 
     /**
-   * Uuid: 69a1458b-92a6-433a-a02d-7beac3ea91f9.
-   */
+     * Uuid: 69a1458b-92a6-433a-a02d-7beac3ea91f9
+     */
     #[TestDox('Substitution cipher -> Can double shift encode')]
-    public function testDoubleShiftEncode(): void
+    public function testSubstitutionCipherCanDoubleShiftEncode(): void
     {
         $cipher = new SimpleCipher('iamapandabear');
         $plaintext = 'iamapandabear';
@@ -107,10 +107,10 @@ class SimpleCipherTest extends TestCase
     }
 
     /**
-   * Uuid: 21d207c1-98de-40aa-994f-86197ae230fb.
-   */
+     * Uuid: 21d207c1-98de-40aa-994f-86197ae230fb
+     */
     #[TestDox('Substitution cipher -> Can wrap on encode')]
-    public function testCipherEncodeWrap(): void
+    public function testSubstitutionCipherCanWrapEncode(): void
     {
         $cipher = new SimpleCipher('abcdefghij');
         $plaintext = 'zzzzzzzzzz';
@@ -118,11 +118,11 @@ class SimpleCipherTest extends TestCase
         $this->assertEquals($ciphertext, $cipher->encode($plaintext));
     }
 
-  /**
-   * Uuid: a3d7a4d7-24a9-4de6-bdc4-a6614ced0cb3.
-   */
+    /**
+     * Uuid: a3d7a4d7-24a9-4de6-bdc4-a6614ced0cb3
+     */
     #[TestDox('Substitution cipher -> Can wrap on decode')]
-    public function testCipherDecodeWrap(): void
+    public function testSubstitutionCipherCanWrapDecode(): void
     {
         $cipher = new SimpleCipher('abcdefghij');
         $plaintext = 'zzzzzzzzzz';
@@ -130,11 +130,11 @@ class SimpleCipherTest extends TestCase
         $this->assertEquals($plaintext, $cipher->decode($ciphertext));
     }
 
-  /**
-   * Uuid: e31c9b8c-8eb6-45c9-a4b5-8344a36b9641.
-   */
+    /**
+     * Uuid: e31c9b8c-8eb6-45c9-a4b5-8344a36b9641
+     */
     #[TestDox('Substitution cipher -> Can encode messages longer than the key')]
-    public function testCanEncodeMessageLongerThanKey(): void
+    public function testSubstitutionCipherCanEncodeMessageLongerThanKey(): void
     {
         $cipher = new SimpleCipher('abc');
         $cipherText = 'iboaqcnecbfcr';
@@ -142,11 +142,11 @@ class SimpleCipherTest extends TestCase
         $this->assertEquals($cipherText, $cipher->encode($plainText));
     }
 
-  /**
-   * Uuid: 93cfaae0-17da-4627-9a04-d6d1e1be52e3.
-   */
+    /**
+     * Uuid: 93cfaae0-17da-4627-9a04-d6d1e1be52e3
+     */
     #[TestDox('Substitution cipher -> Can decode messages longer than the key')]
-    public function testCanDecodeMessageLongerThanKey(): void
+    public function testSubstitutionCipherCanDecodeMessageLongerThanKey(): void
     {
         $cipher = new SimpleCipher('abc');
         $cipherText = 'iboaqcnecbfcr';
